53e54c0e6573db2b7edc9de54c5753f40e49e202
AylaNasiri <ayla.nasiri.dev@gmail.com>
2026-08-08 11:02:16 +0330

    Lesson-02: Learn Utility-First and Utility Classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,30 @@
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-blue-600">Learning Tailwind</h1>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        
+        <div className="w-[600px] bg-slate-800 rounded-3xl shadow-2xl p-16 text-center">
+          
+          <h1 className="text-6xl font-bold text-white mb-8">
+            Tailwind CSS
+          </h1>
+
+          <p className="text-3xl text-slate-300 mb-12">
+            Learning utility classes
+          </p>
+
+          <button className="bg-blue-500 hover:bg-blue-600 text-white text-2xl font-bold px-16 py-5 rounded-2xl shadow-lg">
+            Start Learning
+          </button>
+
+        </div>
+      </div>
     </>
   );
 }
