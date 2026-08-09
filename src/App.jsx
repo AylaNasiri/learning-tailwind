3fcbcda3bea895b89deb945f7e87c434370bd622
AylaNasiri <ayla.nasiri.dev@gmail.com>
2026-08-09 13:04:50 +0330

    Lesson-03: Learn States and Variants

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,66 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-[#ececec] min-h-screen flex items-center justify-center ">
+        <form
+          className="
+                 bg-white
+                 w-96
+                 p-6
+                 rounded-xl
+                 shadow-lg
+                 has-[:focus]:ring-2 
+                 has-[:focus]:ring-blue-400">
         
-        <div className="w-[600px] bg-slate-800 rounded-3xl shadow-2xl p-16 text-center">
-          
-          <h1 className="text-6xl font-bold text-white mb-8">
-            Tailwind CSS
-          </h1>
+            
+          <h1 className="text-black text-2xl font-bold">Sign Up</h1>
 
-          <p className="text-3xl text-slate-300 mb-12">
-            Learning utility classes
+          <input type="email"  required placeholder="Enter your Email address"
+            className="
+              peer
+              w-full 
+              mt-4 
+              p-3
+              border 
+              rounded-lg 
+              placeholder:text-gray-400
+              focus:border-blue-500
+              focus:outline-none
+              invalid:border-red-500
+          "/>
+
+          <p className="
+          
+             hidden
+             peer-invalid:block
+             text-red-500
+             mt-2 
+          ">   
+          
+            Please enter a valid email.
+
           </p>
 
-          <button className="bg-blue-500 hover:bg-blue-600 text-white text-2xl font-bold px-16 py-5 rounded-2xl shadow-lg">
-            Start Learning
+          <button className="
+             
+             mt-6
+             px-6
+             py-3
+             bg-blue-500
+             rounded-lg
+             text-white
+             text-xl
+             hover:bg-blue-600
+             active:bg-blue-800
+             disabled:opacity-50
+
+          
+          ">
+            Submit
           </button>
 
-        </div>
+        </form>
+
       </div>
     </>
   );
